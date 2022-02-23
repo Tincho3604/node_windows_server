03feb538599fe58a917b9d5bdb20eb2a7b64589c
martinc <martinc@fktech.net>
2022-02-23 01:52:27 -0300

    Feat: Create Node Service Window

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require ('express');
 
 const server = express();
-const portA = process.env.PORT || 3000;
 
 const baseLocalOrigin = 'http://localhost:'
 const port = 8081
 
 server.get('/', (req, res) => {
     res.type('text/plain')
-    res.send('Server Expresso')
+    res.send('Server Express')
 })
 
 
-server.listen(port, () => console.log(`Server Expresso running on port ${baseLocalOrigin}${port}`))
+server.listen(port, () => console.log(`Server Express running on port ${baseLocalOrigin}${port}`))
---
+++ nodeService.js
@@ -0,0 +1,44 @@
+const Service = require('node-windows').Service
+
+const svc = new Service({
+    name: "FileWatcher",
+    description: "Service Flex 2.0 FileWatcher",
+    script: "./index.js"
+})
+
+const entryCommand = process.argv[2] && process.argv[2].replace('--','');
+
+const _ENTRY_COMMANDS_ = {
+    'install': install,
+    'unistall': unistall
+};
+
+function install() {
+    svc.on('install', function() {
+        svc.start();
+        console.log("Service was installed");
+    })
+}
+
+
+svc.on('alreadyinstalled', function () {
+    console.log("Service is already installed.");
+})
+svc.on('start', function () {
+})
+
+svc.install()
+
+
+function unistall(){
+    svc.on('unistall', function() {
+        svc.start();
+        console.log("Service was installed")
+    })
+    svc.uninstall();
+}
+function init() {
+    _ENTRY_COMMANDS_[entryCommand] && _ENTRY_COMMANDS_[entryCommand]();
+}
+
+init();
